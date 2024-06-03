ab8580c337d187faf18e8bb15d968bea7b39e517
ibrah <ibrahimnasser07@gmail.com>
2024-06-04 02:36:46 +0300

    added icon.

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,3 +3,4 @@ const String currentTheme = "currentTheme";
 const String currentInterval = "currentInterval";
 const String currentThreshold = "currentThreshold";
 const String minimumSpeed = "minimumSpeed";
+const String bumpsCollection = "bumpsCollection";
